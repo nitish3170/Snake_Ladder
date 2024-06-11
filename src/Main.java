fc8c72488bf01994c2c0552317a6ef083d86cd31
david <david@Nitishs-MacBook-Air-501.local>
2024-06-11 11:53:13 +0530

    UC2 roll dice

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Random;
+import java.util.Vector;
+
 public class Main {
+    private static int RollDice(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(6) + 1;
+        return randomNumber;
+    }
+    private static void UC2_RollDice(){
+        int dice=RollDice();
+        System.out.printf("Dice Roll: %d",dice);
+    }
     public static void main(String[] args) {
 
         System.out.println("Welcome to Snake and Ladder Game");
+        UC2_RollDice();
     }
 }
