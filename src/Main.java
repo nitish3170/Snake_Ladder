b0400bdb826a1e54f03d605a852b4600063a6267
david <david@Nitishs-MacBook-Air-501.local>
2024-06-11 12:23:11 +0530

    UC4 reach 100 to wim

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ public class Main {
     }
     private static void UC2_RollDice(){
         int dice=RollDice();
+        System.out.println("UC2 result: ");
         System.out.printf("Dice Roll: %d \n",dice);
     }
     private static int CheckOption(){
@@ -18,14 +19,36 @@ public class Main {
     }
     private static void UC3_CheckOptions(){
         int i=CheckOption();
+        System.out.println("UC3 result: ");
         if (i==1) System.out.println("Case: No Play");
         else if (i==2) System.out.println("Case: Ladder");
         else System.out.println("Case: Snake");
+    }
+    private static int CalculateValue(int start){
+        int i=RollDice();
+        int option=CheckOption();
+        switch (option){
+            case 1:
+                return start;
+            case 2:
+                if (start+i<=100) return start+i;
+                else return start;
+            case 3:
+                if (start-i<0) return 0;
+                else return start-i;
+        }
+        return 0;
     }
     public static void main(String[] args) {
 
         System.out.println("Welcome to Snake and Ladder Game");
         UC2_RollDice();
         UC3_CheckOptions();
+        System.out.println("Game Play: ");
+        int start=0;
+        while (start!=100){
+            start=CalculateValue(start);
+        }
+        System.out.println("Player Won the game");
     }
 }
